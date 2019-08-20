a2680744c133289720cefffe5709ef7cafcbeeda
e <16670409213@163.com>
2019-08-20 16:05:15 +0800

    security

---
+++ src/main/java/com/example/taxtool/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.taxtool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("nieqin")
+                .password("121453698").roles("ADMIN");
+    }
+
+}
