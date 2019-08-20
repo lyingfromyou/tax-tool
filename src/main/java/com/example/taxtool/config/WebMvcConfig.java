ab0360dcb886a157522d9ab3180b81517450f158
e <16670409213@163.com>
2019-08-20 16:53:45 +0800

    security

---
+++ src/main/java/com/example/taxtool/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.taxtool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/login");
+        registry.addViewController("/tax").setViewName("tax");
+    }
+}
--- src/main/java/com/example/taxtool/config/WebSecurityConfig.java
+++ src/main/java/com/example/taxtool/config/WebSecurityConfig.java
@@ -1,24 +1,43 @@
 package com.example.taxtool.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
+
+    private final static BCryptPasswordEncoder ENCODER = new BCryptPasswordEncoder();
+
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("nieqin")
-                .password("121453698").roles("ADMIN");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()//允许基于HttpServletRequest使用限制访问
+                .antMatchers("/", "/login").permitAll()//不需要身份认证
+                .anyRequest().authenticated()//其他路径必须验证身份
+                .and()
+                .formLogin()
+                .successForwardUrl("/tax")
+                .defaultSuccessUrl("/tax", true)
+                .permitAll()
+                .and()
+                .logout().permitAll().deleteCookies("JSESSIONID");//退出删除cookie
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(ENCODER)
+                .withUser("nieqin")
+                .password(ENCODER.encode("liqian520"))
+                .roles("ADMIN");
+    }
+
 
 }
