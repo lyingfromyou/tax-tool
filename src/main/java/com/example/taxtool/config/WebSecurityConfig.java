e9361ed53c6e360ec3a612cf45fd2a3b21936807
lying <183023840@qq.com>
2019-10-18 17:33:23 +0800

    disable security

--- src/main/java/com/example/taxtool/config/WebSecurityConfig.java
+++ src/main/java/com/example/taxtool/config/WebSecurityConfig.java
@@ -18,27 +18,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()//允许基于HttpServletRequest使用限制访问
-                .antMatchers("/", "/login","/static/**").permitAll()//不需要身份认证
-                .anyRequest().authenticated()//其他路径必须验证身份
-                .and()
-                .formLogin()
-                .successForwardUrl("/tax")
-                .defaultSuccessUrl("/tax", true)
-                .permitAll()
-                .and()
-                .logout().permitAll().deleteCookies("JSESSIONID");//退出删除cookie
-        super.configure(http);
-        http.authorizeRequests().antMatchers("*").permitAll();
+//        http.authorizeRequests()//允许基于HttpServletRequest使用限制访问
+//                .antMatchers("/", "/login","/static/**").permitAll()//不需要身份认证
+//                .anyRequest().authenticated()//其他路径必须验证身份
+//                .and()
+//                .formLogin()
+//                .successForwardUrl("/tax")
+//                .defaultSuccessUrl("/tax", true)
+//                .permitAll()
+//                .and()
+//                .logout().permitAll().deleteCookies("JSESSIONID");//退出删除cookie
+//        super.configure(http);
+//        http.authorizeRequests().antMatchers("*").permitAll();
+        http.authorizeRequests().anyRequest().permitAll().and()
+                .logout().permitAll();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(ENCODER)
-                .withUser("nieqin")
-                .password(ENCODER.encode("liqian520"))
-                .roles("ADMIN");
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(ENCODER)
+//                .withUser("nieqin")
+//                .password(ENCODER.encode("liqian520"))
+//                .roles("ADMIN");
     }
 
 
