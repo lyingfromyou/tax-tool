01fd756867b49b490c8fafd00a607f71a27d45b5
lying <183023840@qq.com>
2020-05-29 20:05:57 +0800

    l

--- src/test/java/com/example/taxtool/ApplicationTest.java
+++ src/test/java/com/example/taxtool/ApplicationTest.java
@@ -4,17 +4,23 @@ import cn.hutool.core.io.FileUtil;
 import com.example.taxtool.service.MinioTemplate;
 import com.example.taxtool.utils.CheckMail;
 import com.example.taxtool.utils.MailUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author by Lying
@@ -25,23 +31,69 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 public class ApplicationTest {
 
+    public static final String key = "IPHONE_11_PRO";
+    public static final CountDownLatch latch = new CountDownLatch(1);
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+    @Test
+    public void test99() throws Exception {
+        redisTemplate.opsForValue().set(key, "10");
+//        redisTemplate.opsForValue().set(key, 10);
+//
+        for (int i = 0; i < 10000; i++) {
+            new Thread(new QiangGou(new User(Long.valueOf(i), i + " 用户"), redisTemplate)).start();
+        }
+        latch.countDown();
+        TimeUnit.SECONDS.sleep(10);
+
+    }
+
+    static class QiangGou implements Runnable {
+        private User user;
+        private  StringRedisTemplate redisTemplate;
+
+
+        public QiangGou(User user, StringRedisTemplate redisTemplate) {
+            this.user = user;
+            this.redisTemplate = redisTemplate;
+        }
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            latch.await();
+            Long i = redisTemplate.opsForValue().decrement(key);
+            if (i >= 0) {
+                System.err.println(user + ": 抢到 ->" + i);
+            }
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    class User {
+        Long userId;
+        String userName;
+    }
+
     @Autowired
     MinioTemplate minioTemplate;
 
     @Test
-    public void test1(){
+    public void test1() {
         String fileName = "11.xlsx";
         try (InputStream inputStream = minioTemplate.getObject("tax", fileName)) {
-            FileUtil.writeFromStream(inputStream, "D:\\tmp\\"+fileName);
+            FileUtil.writeFromStream(inputStream, "D:\\tmp\\" + fileName);
         } catch (Exception e) {
             log.error("文件读取异常", e);
         }
     }
 
     @Test
-    public void test2(){
-            List<String> mails = CheckMail.check(Arrays.asList("984945343@qq.com", "941185245@qq.com"));
-            System.err.println(mails);
+    public void test2() {
+        List<String> mails = CheckMail.check(Arrays.asList("984945343@qq.com", "941185245@qq.com"));
+        System.err.println(mails);
     }
 
 
